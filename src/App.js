eca8891455682b907ba80549f7126338ef3f6df4
daniela <moreno.tobar.daniela@gmail.com>
2022-02-16 11:07:43 -0500

    api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import './App.css';
 import { NavBar } from './Views/NavBar';
+import React from "react";
+import './App.css';
+class App extends React.Component {
 
-function App() {
-  return (
-    <NavBar/>
-    
-  );
+    // Constructor
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: [],
+            DataisLoaded: false
+        };
+    }
+
+    // ComponentDidMount is used to
+    // execute the code
+    componentDidMount() {
+        fetch("https://www.etnassoft.com/api/v1/get/?category=libros_programacion&criteria=most_viewed%22")
+            .then((res) => res.json())
+            .then((json) => {
+                this.setState({
+                    items: json,
+                    DataisLoaded: true
+                });
+            })
+    }
+    render() {
+        const { DataisLoaded, items } = this.state;
+        if (!DataisLoaded) return <div>
+            <h1> Pleses wait some time.... </h1> </div> ;
+
+        return (
+        <div className = "App">
+            <h1> Fetch data from an api in react </h1> {
+                items.map((item) => (
+                <ol key = { item.id } >
+                    Titulo: { item.title },
+                    Autor: { item.author },
+                    Contenido: { item.content }
+                    </ol>
+                ))
+            }
+        </div>
+    );
+}
 }
 
 export default App;
